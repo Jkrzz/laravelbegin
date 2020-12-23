f077e369289486db10541088aa44fd8989c39ce5
JKRZ <heinhtetjkrz@gmail.com>
2020-12-23 19:59:20 +0630

    01 route

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include ('layouts/head')
+</head>
+<body>
+    @include ('layouts/navigation')
+    @yield ('content')
+    @include ('layouts/footer')
+    @include ('layouts/script')
+</body>
+</html>
