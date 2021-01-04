b4ac6c77195814b4c4f5e77a2c557f74039f3231
JKRZ <heinhtetjkrz@gmail.com>
2021-01-04 17:50:58 +0630

    03_view

---
+++ resources/views/post.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        Post Page
+    </h1>
+    <p>
+        ID:{{$id}}
+        Name:{{$name}}
+    </p>
+</body>
+</html>
